b7cef56ac313283a184e3ae6ea71c5bf02192108
Alex Teghipco <alex.teghipco@uci.edu>
2024-03-11 13:13:15 -0400

    Add files via upload

--- demo_start_here.m
+++ demo_start_here.m
@@ -1,6 +1,6 @@
 %% Example 1: FDR corrected 1-tail mediation analysis
 %% Extract behavioral data
-beh = readtable('/home/alex/Downloads/abcBeh.csv');
+beh = readtable('./abcBeh.csv');
 subs = beh.Subject;
 moca  = beh.TotalScore;
 age = beh.Age;
@@ -10,22 +10,23 @@ tor = [];
 for i = 1:length(subs)
     disp(num2str(i))
     try
-        f = ls([pwd '/mri/smwp1' subs{i} '.nii.gz']);
-        f = f(1:end-1);
-        tmp = load_nifti(f);
+        f = ls([pwd '\mris\smwp1' subs{i} '.nii.gz']);
+        f = f(1:end);
+        tmp = load_nifti([pwd '\mris\' f]);
         gm(:,:,i) = tmp.vol(:);
     catch
         gm(:,:,i) = 0;
         tor = [tor; i];
     end
 end
+
 tor2 = [];
 for i = 1:length(subs)
     disp(num2str(i))
     try
-        f = ls([pwd '/mri/smwp2' subs{i} '.nii.gz']);
-        f = f(1:end-1);
-        tmp = load_nifti(f);
+        f = ls([pwd '\mris\smwp2' subs{i} '.nii.gz']);
+        f = f(1:end);
+        tmp = load_nifti([pwd '\mris\' f]);
         wm(:,:,i) = tmp.vol(:);
     catch
         wm(:,:,i) = 0;
@@ -43,6 +44,7 @@ wm(tor,:) = [];
 subs(tor) = [];
 age(tor) = [];
 moca(tor) = [];
+beh(tor,:) = [];
 
 % get size of image so we can reverse image indexing
 sz = size(tmp.vol);
@@ -60,6 +62,27 @@ tmp.vol = zeros(size(tmp.vol));
 tmp.vol(wmid) = 1;
 save_nifti(tmp,'mask_wm.nii.gz')
 
+
+% MORE SANITY
+[ctmp,iatmp,ibtmp] = intersect(tmpp.subs(tmpp.kid),app.inBeh.Subject);
+corr(tmpp.age(tmpp.kid(iatmp)),app.inBeh.Age(ibtmp))
+corr(tmpp.moca(tmpp.kid(iatmp)),app.inBeh.TotalScore(ibtmp))
+corr(tmpp.tiv(tmpp.kid(iatmp)),app.inBeh.TIV(ibtmp)) % FIX TIV IN abcBeh.csv
+
+for ttt = 1:length(iatmp)
+    rtmp(ttt,1) = corr(tmpp.gm(tmpp.kid(iatmp(ttt)),:)',app.inBrain(ibtmp(ttt),:)'); % FIX TIV IN abcBeh.csv
+end
+
+for i = 1:length(insub)
+    id = find(ismember(tivbeh.Var1,insub{i}));
+    if isempty(id)
+        tivfin(i,:) = NaN;
+    else
+        tivfin(i,:) = tivbeh.CSF_Total(id);
+    end
+end
+
+
 % sanity check "significant" relationships by correlating variables to
 % brain data...moca and age separately
 [r,p] = corr(gm(:,gmid),moca);
@@ -74,9 +97,10 @@ length(find(mafdr(p,'BHFDR',true)))./length(gmid)
 length(find(mafdr(p2,'BHFDR',true)))./length(wmid)
 
 % Extract total intracranial volume (nuissance variable)
-tivd = readtable('/home/alex/Downloads/TIVDataABC.xlsx');
+tivd = readtable('TIVDataABC.xlsx');
 [c,ia,ib] = intersect(tivd.Var1,subs);
 subs = subs(ib);
+beh = beh(ib,:);
 moca = moca(ib);
 age = age(ib);
 wm = wm(ib,:);
@@ -90,6 +114,9 @@ csfv = tivd.CSF_Total(ia);
 
 % save our inputs in case we need to reload
 save('input_age_moca.mat','-v7.3')
+
+
+%[ctmp,iatmp,ibtmp] = intersect(tmp.beh.Subject(tmp.kid),app.inBeh);
 
 % Subjects 133 through 243 tested COVID positive and the relationship
 % between age and moca is weaker in this group...we will ignore it for now.
@@ -125,7 +152,7 @@ length(find(oP_gm_med < 0.05)) % 0.7749
 % load in ROI names in atlas and their corresponding values in the atlas
 labs = readtable('jhu.txt');
 % load in the atlas
-roiin = load_nifti('rjhu_resampled_to_FSL_fixed.nii');
+roiin = load_nifti('rjhu.nii');
 
 % loop over unique values in the atlas and extract voxel identities for
 % each
@@ -136,24 +163,28 @@ end
 % now get mean morphometry within each ROI. Because our atlas has GM and WM
 % regions, if the ROI is in GM, we pull from the gm morphometry data and
 % same for WM. We ignore CSF (e.g., ventricles). 
-kid = 1:132; % this is ABC folks (i.e., non-covid positive cohort)
+kid = 1:131 %1:133; % this is ABC folks (i.e., non-covid positive cohort)
 for i = 1:height(labs) % loop over ROIs
     disp(num2str(i))
     if labs.Var4(i) == 1 % if GM ROI
         [c,ia,ib] = intersect(rois{i},gmid); % find grey matter voxel indices that overlap with the roi
         tmp = gm(kid,ib); % extract ABC folks' corresponding overlapping voxels
-        zc = find(all(tmp == 0, 1)); % remove any voxels that are 0 across subjects (i.e., ROIs not overlapping with the voxelwise maps for example because they intrude on another tissue type)
-        tmp(:,zc) = []; % remove those voxels
-        morph(:,i) = mean(tmp,2); % get mean across voxels for the ROI-level morphometry, for each participant
+       % zc = find(all(tmp == 0, 1)); % remove any voxels that are 0 across subjects (i.e., ROIs not overlapping with the voxelwise maps for example because they intrude on another tissue type)
+     %   tmp(:,zc) = []; % remove those voxels
+        morph(:,i) = mean(tmp,2,'omitnan'); % get mean across voxels for the ROI-level morphometry, for each participant
     elseif labs.Var4(i) == 2 % if WM ROI
         [c,ia,ib] = intersect(rois{i},wmid); % same as above...
         tmp = wm(kid,ib);
-        zc = find(all(tmp == 0, 1));
-        tmp(:,zc) = [];
-        morph(:,i) = mean(tmp,2);
+       % zc = find(all(tmp == 0, 1));
+      %  tmp(:,zc) = [];
+        morph(:,i) = mean(tmp,2,'omitnan');
     end
     %rois{i} = find(roiin.vol == labs.Var1(i));
 end
+
+% try something else--no removal...
+tmp = zeros(sz);
+tmp(gmid) = 
 
 % remove bad ROIs--for example, ROIs with a mean of 0 across all subjects
 % (these are CSF ROIs, which our if statements ignore) or ROIs with NaNs
@@ -170,7 +201,9 @@ rois(tor) = [];
 
 % Run ROI-level analysis. NOW we can do the fancy modeling--mediation with
 % robust OLS regression and bootstrapped p-values. 
-[oSv_wm_med,oSm_wm_med,oR_wm_med,oP_wm_med,~,~,~,~,~,cclMxPPos_wm_med,cclMxPNeg_wm_med] = vbm(age(kid),moca(kid),morph,[tiv(kid)],'mediation',0,'default',0.05,[],[],[],false,false,[{'boot','robust'}],false,'both');  % [tiv wmv gmv csfv cnc']
+ [oSv,oSm,oR,oRm,oP,oPc,oPm,oPcm,oPth,oPall,oPthStd,oPthm,oPallm,oPthStdm,ccIds,pcSurvPos,pcSurvNeg,cclMxPPos,cclMxPNeg] = vbm(age(kid),moca(kid),morph,[tiv(kid)],'mediation',0,'default',0.05,[],[],[],false,false,[{'boot','robust'}],false,'both',false,0,false)
+%[oSv_wm_med,oSm_wm_med,oR_wm_med,oP_wm_med,~,~,~,~,~,cclMxPPos_wm_med,cclMxPNeg_wm_med] = vbm(age(kid),moca(kid),morph,[tiv(kid)],'mediation',0,'default',0.05,[],[],[],false,false,[{'boot','robust'}],false,'both');  % [tiv wmv gmv csfv cnc']
+%[oSv_wm_med,oSm_wm_med,oR_wm_med,oP_wm_med,~,~,~,~,~,cclMxPPos_wm_med,cclMxPNeg_wm_med] = vbm(age(kid),moca(kid),morph,[tiv(kid)],'mediation',0,'default',0.05,[],[],[],false,false,[{'boot','robust'}],false,'both',true,1000);  % [tiv wmv gmv csfv cnc']
 
 % Again, we manually FDR-correct values so we have both uncorrected and
 % corrected values available. Also apply an FDR threshold 
